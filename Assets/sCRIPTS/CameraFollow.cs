62cc692e9a4b870766b54eb595270e9696f550b3
Matthew Redington <mredington4@gatech.edu>
2020-07-11 12:01:58 -0400

    level design and movement

---
+++ Assets/sCRIPTS/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float RoationSpeed = 1f;
+    public Transform Target;
+    public Transform Player;
+
+    float mouseX, mouseY;
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        mouseY += Mathf.Clamp(Input.GetAxis("Mouse Y") * RoationSpeed, -35, 60);
+        mouseX += Input.GetAxis("Mouse X") * RoationSpeed;
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+
+    }
+}
---
+++ Assets/sCRIPTS/CharacterControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class CharacterControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator Anim;
+
+    private void Awake()
+    {
+        Anim = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Turn();
+        //Movement();
+        Move();
+        Jump();
+    }
+
+    void Turn()
+    {
+        Anim.SetFloat("Turning", Input.GetAxis("Horizontal"));
+        Debug.Log(Input.GetAxis("Horizontal"));
+    }
+    void Movement()
+    {
+        
+        Anim.SetFloat("Forward", Input.GetAxis("Vertical"));
+
+    }
+
+    void Move()
+    {
+        if (Anim.GetBool("Walk")){
+            Anim.SetFloat("MoveZ", Mathf.Clamp(Input.GetAxis("MoveZ"), -0.25f, 0.25f));
+            Anim.SetFloat("Movex", Mathf.Clamp(Input.GetAxis("MoveX"), -0.25f, 0.25f));
+        }
+        else
+        {
+            Anim.SetFloat("MoveZ",(Input.GetAxis("MoveZ")));
+            Anim.SetFloat("MoveX", (Input.GetAxis("MoveX")));
+        }
+
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Anim.SetTrigger("Jump");
+        }
+
+    }
+}
